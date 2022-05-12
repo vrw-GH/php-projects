66a81940fb671000283d849a4b7125250e643c76
Victor Wright <victor.wright@outlook.de>
2022-05-12 15:11:44 +0200

    initial setup
    Root htdocs

--- index.php
+++ index.php
@@ -9,30 +9,31 @@
 //	header("Refresh:10; url=https://www.websparrow.org");
 
  	echo "<h1>my Web Server @ $server $port </h1>";
-	echo "$version";
+	echo 'This site is running '."$version";
 	echo "<br>";
-	echo '<a href="/index.html">index.html</a>';
+	echo '<a href="/index.html">Apache Test Page</a>';
 	echo "<br>";
 //	echo "PHP version    : $phpVersion </br>"; 
+	echo '<a href="/info.php">PHP Info</a>';
+	echo "<br>";
 	echo "<br>";
 	echo '<a href="/phpmyadmin">PHPMyAdmin</a>';
 	echo "<br>";
-	echo '<a href="/info.php">info.php</a>';
 	echo "<br>";
-	echo "<br>";
+	echo '<a href="/projects/myprojects.php">My Projects</a>';
 	echo "<hr>";
 
-	echo 'Documentation & Guides:';
+	echo 'Documentation & Guides:  (opens in new windows)';
 	echo "<br>";
-	echo '<a href="https://www.php.net/docs.php" target="_blank">PHP</a>';
+	echo '<a href="https://www.php.net/docs.php" target="_blank">php.net/docs</a>';
 	echo "<br>";
-	echo '<a href="http://localhost/phpmyadmin/doc/html/index.html" target="_blank">PhpMyAdmin</a>';
+	echo '<a href="http://localhost/phpmyadmin/doc/html/index.html" target="_blank">phpmyadmin/docs</a>';
 	echo "<br>";
-	echo '<a href="https://httpd.apache.org/docs/" target="_blank">Apache</a>';
+	echo '<a href="https://httpd.apache.org/docs/" target="_blank">apache.org/docs</a>';
 	echo "<br>";
-	echo '<a href="https://www.websparrow.org/misc/how-to-install-apache-2-4-and-php-8-on-a-windows-machine" target="_blank">www.websparrow.org</a>';
+	echo '<a href="https://www.websparrow.org/misc/how-to-install-apache-2-4-and-php-8-on-a-windows-machine" target="_blank">websparrow.org/how_to</a>';
 	echo "<br>";
-	echo '<a href="https://getcomposer.org/doc/00-intro.md" target="_blank">Composer</a>';
+	echo '<a href="https://getcomposer.org/doc/00-intro.md" target="_blank">composer.org/docs</a>';
 	echo "<br>";
  
  ?>
---
+++ projects/myprojects.php
@@ -0,0 +1,33 @@
+<?php
+echo '<h1> My Projects </h1> ';
+echo "<hr>";
+
+$path = '.';
+// Check directory exists or not
+if(file_exists($path) && is_dir($path)){
+   // Scan the files in this directory
+   $result = scandir($path);
+   // Filter out the current (.) and parent (..) directories
+   $files = array_diff($result, array('.', '..'));
+   if(count($files) > 0){
+      // Loop through retuned array
+      foreach($files as $file){
+         if(!is_file("$path/$file")){
+            // Display Project link            
+            // $a_link = '<a href="/projects/'.$file.'"> '.$file.' </a>';
+            // echo $a_link;
+            echo '<a href="/projects/'.$file.'"> '.$file.' </a>';
+            echo '<br>';
+         } else {
+            // do nothing
+         }
+      }
+   } else{
+      echo "No Projects found.";
+   }
+} else {
+   echo "ERROR: The directory does not exist.";
+}
+echo '<br>';
+echo "------------ End Of List ------------".'<br>'.'<br>'.'<br>'.'<br>';
+echo '<a href="/projects">List Projects Directory</a>';
